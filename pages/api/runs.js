5dd94b42ee230582c525e80669f67bc34bbbd656
murraycopps <murraycopps@gmail.com>
2022-12-13 08:22:24 -0500

    fixed header

--- pages/api/runs.js
+++ pages/api/runs.js
@@ -7,11 +7,10 @@ export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("running");
 
-  header = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-    "Access-Control-Allow-Headers": "Content-Type",
-  };
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
 
   const bodyObject = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
 
