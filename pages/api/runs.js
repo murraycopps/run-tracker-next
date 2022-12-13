9d8d5a41aebfdbcade6d73510b14422f56b5daf3
murraycopps <murraycopps@gmail.com>
2022-12-13 08:16:09 -0500

    convert req.body to object if it is not an object

--- pages/api/runs.js
+++ pages/api/runs.js
@@ -7,11 +7,10 @@ export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("running");
 
-  console.log(req.method, req.body);
+const bodyObject = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
 
   switch (req.method) {
     case "POST":
-      let bodyObject = JSON.parse(req.body);
       await db.collection("runs").insertOne(bodyObject);
       break;
     case "GET":
@@ -19,12 +18,10 @@ export default async function handler(req, res) {
       res.json({ status: 200, data: allPosts });
       break;
     case "DELETE":
-      let bodyObject2 = req.body;
-      await db.collection("runs").deleteOne({ "_id": new ObjectId(bodyObject2._id)});
+      await db.collection("runs").deleteOne({ "_id": new ObjectId(bodyObject._id)});
       break;
     case "PUT":
-      let bodyObject3 = req.body;
-      const { _id, ...rest } = bodyObject3;
+      const { _id, ...rest } = bodyObject;
 
       await db.collection("runs").updateOne({ "_id": new ObjectId(_id) }, { $set: { ...rest } });
       break;
